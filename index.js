ad34c461f5d899acefa743c08456f0fc3ab772b5
Andrew Love <andrew.love@ajbell.co.uk>
2022-11-14 10:31:14 +0000

    Add CORS headers

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 19998
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
